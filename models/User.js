2b2afdb81cbe9d5a9addcfb4f6775d148cf69dba
Brayan-chan <al071392@uacam.mx>
2025-01-27 02:26:40 -0600

    Agregar campo 'matricula' al esquema de usuario

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
-    phone: { type: String, required: true }
+    phone: { type: String, required: true },
+    matricula: { type: String, required: true }
 });
 
 const User = mongoose.model('User', userSchema);
