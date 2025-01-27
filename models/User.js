b24ab71ac17ce06a44bdaf0a9529e26e21921c60
Brayan-chan <al071392@uacam.mx>
2025-01-27 08:44:37 -0600

    Agregar comentarios y mejorar la definición del esquema del modelo de usuario

--- models/User.js
+++ models/User.js
@@ -1,12 +1,16 @@
+// Importar mongoose
 import mongoose from 'mongoose';
 
+// Definir el esquema del usuario
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    phone: { type: String, required: true },
-    matricula: { type: String, required: true }
+    name: { type: String, required: true }, // Campo nombre, requerido
+    email: { type: String, required: true }, // Campo email, requerido
+    phone: { type: String, required: true }, // Campo teléfono, requerido
+    matricula: { type: String, required: true } // Campo matrícula, requerido
 });
 
+// Crear el modelo de usuario
 const User = mongoose.model('User', userSchema);
 
+// Exportar el modelo de usuario
 export default User;
